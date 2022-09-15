77484300bf606b77e8f5b8fe5216d4f630bdfddc
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2022-09-15 17:10:18 -0400

    Creating login and register views

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
-
-
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 const routes = [
-  
+  {path:'/login',component:Login},
+  {path:'/register',component:Register}
 ];
 
 const history = createWebHistory();
