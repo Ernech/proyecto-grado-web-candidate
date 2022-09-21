770cc71bf53383e567174d2e11a22c3d22f10e86
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2022-09-20 22:47:09 -0400

    Decoding token

---
+++ src/helpers/get-token-info.js
@@ -0,0 +1,19 @@
+import jwt_decode from "jwt-decode";
+const getDecodedToken=()=>{
+    if(localStorage.getItem('token')){
+        const token = localStorage.getItem('token').split(' ')
+        const decoded= jwt_decode(token[1])
+        return decoded;
+    }
+    return null
+}
+const getUserId=()=>{
+    if(getDecodedToken()){
+        const tokenDecoded = getDecodedToken()
+        return tokenDecoded.id
+    }
+    return '';
+}
+
+
+export default{getUserId}
